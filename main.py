6f2a107a12dbe4496c84e5862c1d334b67af5f1c
Vivi Rosita <vivirosita.r@gmail.com>
2021-11-20 16:26:18 +0700

    create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+how to manipulate strings in Python
+
+"""
+my_string = "Sometimes life is unfair"
+new_string = my_string.replace("unfair", "fair enough")
+
+print(my_string.find("Sometimes", 1, 20))
+print(new_string)
+print(new_string.upper())
+print(int("78"))
+print(int(34.5))
+print(ord('g'))
+print(bool("hi"))
