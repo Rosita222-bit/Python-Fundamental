78160b0cae30805f0bf6dcfc94e173f3762513f1
Vivi Rosita <vivirosita.r@gmail.com>
2021-11-24 13:45:31 +0700

    create basic.py

---
+++ basic.py
@@ -0,0 +1,8 @@
+"""
+This chapter 1 from Python Crash course book
+"""
+
+#Try it yourself
+
+message = "Good morning!"
+print(message)
--- main.py
+++ main.py
@@ -12,3 +12,14 @@ print(int("78"))
 print(int(34.5))
 print(ord('g'))
 print(bool("hi"))
+
+num_list = [4,5,7,8,9,1,2]
+
+triple_list = map(lambda n: n * 3, num_list)
+
+print(list(triple_list))
+
+numblist = [4, 4,3,7,8,5,6]
+
+less_than_7 = list(filter(lambda  n: n < 7, numblist))
+print(less_than_7)
