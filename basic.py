ffc43cd6843ff950527ede5d8c7c4d365d68f2d9
Vivi Rosita <vivirosita.r@gmail.com>
2021-11-25 05:46:55 +0700

    create basic.py

--- basic.py
+++ basic.py
@@ -68,8 +68,41 @@ print (y)
 print( 4+4)
 
 #Try it yourself 4 comment
-import this
+
 
 #Introducing Lists 3
+secret_number = ['lea', 'denise', 'zuu', 'dita', 'jinni', 'soodam', 'minji']
+print(secret_number[1].title())
+print(secret_number[4])
+print(secret_number[-4])
+print(f"The main dancer of secret number is {secret_number[-4].title()}")
+
+#Try it yourself 1
+itzy = ['ryujin', 'chaeyoung', 'yeji', 'lia', 'yuna']
+greeting0 = f"hello, I am {itzy[0].title()}"
+greeting1 = f"hello, I am {itzy[1].title()}"
+greeting2 = f"hello, I am {itzy[2].title()}"
+greeting3 = f"hello, I am {itzy[3].title()}"
+greeting4 = f"hello, I am {itzy[4].title()}"
+print(greeting0)
+print(greeting1)
+print(greeting2)
+print(greeting3)
+print(greeting4)
+
+cars = ['jazz', 'picanto', 'xenia', 'lamborghini', 'tesla']
+cars.append('innova')
+print(cars)
+cars.insert(2, 'swift')
+print(cars)
+del cars[0]
+print(cars)
+
+popped_cars =cars.pop()
+print(popped_cars)
+cars.remove("xenia")
+print(cars)
+
+#Try it yourself 2
 
 
