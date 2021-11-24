bc86354497fd3b86037a9f45ed9eab906957a4d3
Vivi Rosita <vivirosita.r@gmail.com>
2021-11-24 20:44:20 +0700

    create basic.py

--- basic.py
+++ basic.py
@@ -2,7 +2,70 @@
 This chapter 1 from Python Crash course book
 """
 
-#Try it yourself
+#Try it yourself 1
 
 message = "Good morning!"
 print(message)
+
+message = "Good night!"
+print(message)
+
+name = " ini lovelace "
+print(name.title())
+
+name = "Eternal Love"
+print(name.upper())
+print(name.lower())
+
+first_name = "vivi"
+last_name = "Rosita"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+print(f"Hello, {full_name.title()}")
+
+print("Blockchain")
+print("\tBlockChain")
+
+print("Languages : \nPython\nJava\nJavaScript")
+my_language = "Indonesian "
+my_language.rstrip()
+print(my_language.rstrip())
+
+sentence = "Python's strengths is its diverse community"
+print(sentence)
+
+#Try it yourself 2
+name = "Vivi"
+message1 = f"Hello {name}, would you like to learn some Python today?"
+print(message1)
+
+name1 = "vivi rosita romadhoni"
+name2 = name1.lower()
+name3 = name1.upper()
+name4 = name1.title()
+
+print(name1)
+print(name2)
+print(name3)
+print(name4)
+
+famous_person = "Bretman"
+quote = f"{famous_person} once said, 'if there is no turtle, there is no turtle'"
+print(quote)
+
+name_asho = '\tAshoka Avalokita \nSri Narendra '
+name1_asho = name_asho.lstrip()
+print(name1_asho)
+name2_asho = name_asho.rstrip()
+print(name2_asho)
+
+a = 3
+b = 5.0
+y = a + b
+print (y)
+
+#Try it yourslef3
+print( 4+4)
+
+#Try it yourself 4 comment
+import this
